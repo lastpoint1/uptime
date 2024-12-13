b660cd64c04b8224dfe8b3e2ac0248ff17470030
lastpoint1 <120954568+lastpoint1@users.noreply.github.com>
2024-12-14 01:28:08 +0300

    Delete client.js

--- client.js
+++
@@ -1,6 +0,0 @@
-const { Client, Partials } = require("discord.js");
-
-const partials = Object.values(Partials).filter((x) => typeof x === "string");
-const client = new Client({ intents: 131071, partials, shards: "auto" });
-
-module.exports = { client };
